eb8c69140b44f939e999bea75cccbf2120584a07
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-04-19 19:58:05 +0900

    Before We Start

--- src/react_tutorial/core.cljs
+++ src/react_tutorial/core.cljs
@@ -4,8 +4,61 @@
 ;; -------------------------
 ;; Views
 
+(defn square []
+  [:button.square
+   ;; TODO
+   ])
+
+(defn board []
+  (letfn [(render-square [i]
+            [square])]
+    (let [status "Next player: X"]
+      [:div
+       [:div.status status]
+       [:div.board-row
+        [render-square 0]
+        [render-square 1]
+        [render-square 2]]
+       [:div.board-row
+        [render-square 3]
+        [render-square 4]
+        [render-square 5]]
+       [:div.board-row
+        [render-square 6]
+        [render-square 7]
+        [render-square 8]]])))
+
+(defn game []
+  [:div.game
+   [:div.game-board
+    [board]]
+   [:div.game-info
+    [:div
+     ;; status
+     ]
+    [:ol
+     ;; TODO
+     ]]])
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [game])
+
+(defn calculate-winner [squares]
+  (let [lines [[0 1 2]
+               [3 4 5]
+               [6 7 8]
+               [0 3 6]
+               [1 4 7]
+               [2 5 8]
+               [0 4 8]
+               [2 4 6]]]
+    (reduce (fn [_ [a b c]]
+              (when (and (squares a)
+                         (= (squares a) (squares b))
+                         (= (squares a) (squares c)))
+                (reduced (squares a))))
+            nil
+            lines)))
 
 ;; -------------------------
 ;; Initialize app
