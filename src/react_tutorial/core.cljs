2c8be08363a22a4d200a45fbd51922451168bc49
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-04-22 16:39:44 +0900

    Storing a History

--- src/react_tutorial/core.cljs
+++ src/react_tutorial/core.cljs
@@ -25,56 +25,75 @@
             nil
             lines)))
 
-(defn board []
-  (let [state (reagent/atom {:squares (vec (repeat 9 nil))
-                             :x-is-next? true})]
+(defn board [& {:keys [squares on-click]}]
+  (letfn [(render-square [i]
+            [square
+             :value (squares i)
+             :on-click #(on-click i)])]
+    [:div
+     [:div.board-row
+      (render-square 0)
+      (render-square 1)
+      (render-square 2)]
+     [:div.board-row
+      (render-square 3)
+      (render-square 4)
+      (render-square 5)]
+     [:div.board-row
+      (render-square 6)
+      (render-square 7)
+      (render-square 8)]]))
+
+(defn game []
+  (let [state (reagent/atom {:history [{:squares (vec (repeat 9 nil))}]
+                             :x-is-next? true
+                             :step-number 0})]
     (letfn [(handle-click [i]
-              (let [{:keys [squares x-is-next?]} @state]
+              (let [{:keys [history x-is-next? step-number]} @state
+                    history (vec (take (inc step-number) history))
+                    current (nth history (dec (count history)))
+                    squares (:squares current)]
                 (when-not (or (calculate-winner squares)
                               (squares i))
-                  (swap! state #(-> %
-                                    (assoc-in
-                                     [:squares i]
-                                     (if x-is-next? "X" "O"))
-                                    (assoc
-                                     :x-is-next?
-                                     (not x-is-next?)))))))
-            (render-square [i]
-              [square
-               :value (get-in @state [:squares i])
-               :on-click #(handle-click i)])]
+                  (swap! state assoc
+                         :history (conj history
+                                        {:squares (assoc
+                                                   squares
+                                                   i
+                                                   (if x-is-next? "X" "O"))})
+                         :x-is-next? (not x-is-next?)
+                         :step-number (count history)))))
+            (jump-to [step]
+              (swap! state assoc
+                     :x-is-next? (even? step)
+                     :step-number step))]
       (fn []
-        (let [winner (calculate-winner (:squares @state))
+        (let [history (:history @state)
+              current (nth history (:step-number @state))
+              winner (calculate-winner (:squares current))
               status (if winner
                        (str "Winner: " winner)
                        (str "Next player: "
-                            (if (:x-is-next? @state) "X" "O")))]
-          [:div
-           [:div.status status]
-           [:div.board-row
-            (render-square 0)
-            (render-square 1)
-            (render-square 2)]
-           [:div.board-row
-            (render-square 3)
-            (render-square 4)
-            (render-square 5)]
-           [:div.board-row
-            (render-square 6)
-            (render-square 7)
-            (render-square 8)]])))))
-
-(defn game []
-  [:div.game
-   [:div.game-board
-    [board]]
-   [:div.game-info
-    [:div
-     ;; status
-     ]
-    [:ol
-     ;; TODO
-     ]]])
+                            (if (:x-is-next? @state) "X" "O")))
+              moves (map-indexed (fn [move _]
+                                   (let [desc (if (zero? move)
+                                                (str "Game start")
+                                                (str "Move #" move))]
+                                     [:li {:key move}
+                                      [:a {:href "#"
+                                           :on-click #(jump-to move)}
+                                       desc]]))
+                                 history)]
+          [:div.game
+           [:div.game-board
+            [board
+             :squares (:squares current)
+             :on-click handle-click]]
+           [:div.game-info
+            [:div
+             status]
+            [:ol
+             moves]]])))))
 
 (defn home-page []
   [game])
