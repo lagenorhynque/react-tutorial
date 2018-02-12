f90b4073b7c03bbfbd7cbc162a709a19f8c89242
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-12 14:21:34 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -4,14 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.495" :scope "provided"]
-                 [reagent "0.6.1"]]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
+                 [reagent "0.7.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-figwheel "0.5.9"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.14"]]
 
   :min-lein-version "2.5.0"
+
+  :jvm-opts ["--add-modules" "java.xml.bind"]
 
   :clean-targets ^{:protect false}
   [:target-path
@@ -49,6 +51,6 @@
 
   :aliases {"release" ["do" "clean" ["cljsbuild" "once" "release"]]}
 
-  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.9"]
-                                  [org.clojure/tools.nrepl "0.2.12"]
-                                  [com.cemerick/piggieback "0.2.2-SNAPSHOT"]]}})
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.14"]
+                                  [org.clojure/tools.nrepl "0.2.13"]
+                                  [com.cemerick/piggieback "0.2.2"]]}})
