9a4e8a80d963796c56030228fcf0cfa2f7967e59
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-05-11 16:50:57 +0900

    Making an Interactive Component

--- src/react_tutorial/core.cljs
+++ src/react_tutorial/core.cljs
@@ -2,13 +2,15 @@
   (:require [goog.dom :as gdom]
             [reagent.core :as reagent]))
 
-(defn square [& {:keys [value]}]
-  [:button.square
-   value])
+(defn square []
+  (let [value (reagent/atom nil)]
+    (fn []
+      [:button.square {:on-click #(reset! value "X")}
+       @value])))
 
 (defn board []
   (letfn [(render-square [i]
-            [square :value i])]
+            [square])]
     (let [status "Next player: X"]
       [:div
        [:div.status status]
