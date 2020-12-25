06ba20ae6237481c0c7834335ed96cd74c15bf5b
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-12-26 02:54:09 +0900

    Setup for the Tutorial

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,12 +1,48 @@
 (ns app.core
   "This namespace contains your application and is the entrypoint for 'yarn start'."
-  (:require [reagent.core :as r]
-            [app.hello :refer [hello]]))
+  (:require
+   [reagent.core :as reagent]))
+
+(defn square []
+  [:button.square
+   ;; TODO
+   ])
+
+(defn board []
+  (letfn [(render-square [i]
+            [square])]
+    (let [status "Next player: X"]
+      [:div
+       [:div.status status]
+       [:div.board-row
+        (render-square 0)
+        (render-square 1)
+        (render-square 2)]
+       [:div.board-row
+        (render-square 3)
+        (render-square 4)
+        (render-square 5)]
+       [:div.board-row
+        (render-square 6)
+        (render-square 7)
+        (render-square 8)]])))
+
+(defn game []
+  [:div.game
+   [:div.game-board
+    [board]]
+   [:div.game-info
+    [:div
+     ;; status
+     ]
+    [:ol
+     ;; TODO
+     ]]])
 
 (defn ^:dev/after-load render
   "Render the toplevel component for this app."
   []
-  (r/render [hello] (.getElementById js/document "app")))
+  (reagent/render [game] (.getElementById js/document "app")))
 
 (defn ^:export main
   "Run application startup logic."
